7c1ab431c9b35ca37a9b8146f1b33a2577bba497
ab2648 <ab2648@ARU-S-F4LTRN3>
2025-10-21 09:33:51 +0100

    Add project files.

---
+++ software engineering/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace software_engineering.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
