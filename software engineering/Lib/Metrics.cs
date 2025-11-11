66d23fed5f1707eefaa064a064e7568168ddb68f
wc296 <wc296@ARU-S-55LTRN3>
2025-11-11 10:51:42 +0000

    feat: add pressure matrix data structure, split sample data into matrices

--- software engineering/Lib/Metrics.cs
+++ software engineering/Lib/Metrics.cs
@@ -1,15 +1,20 @@
 ﻿using Microsoft.VisualBasic.FileIO;
 using System.Diagnostics;
+using System.Globalization;
 
 namespace software_engineering.Lib
 {
     public class Metrics
     {
-        protected List<List<short>> data = new List<List<short>>();
+        public List<PressureMatrix> matrices = new List<PressureMatrix>();
 
-        public Metrics(string dataFilePath)
+        // Assumes file name convention as provided by Sample data:
+        // <User ID>_<Timestamp yyyyMMdd>.csv
+        public Metrics(string dataFilePath, int matrixHeight = 32)
         {
-            var parser = new TextFieldParser(dataFilePath);
+            List<List<short>> rows = new List<List<short>>();
+
+            TextFieldParser parser = new TextFieldParser(dataFilePath);
 
             parser.TextFieldType = FieldType.Delimited;
             parser.SetDelimiters(",");
@@ -19,18 +24,44 @@ namespace software_engineering.Lib
                 string[]? fields = parser.ReadFields();
                 if (fields == null) continue;
 
+                rows.Add(fields.Select(short.Parse).ToList());
 
-                data.Add(fields.Select(short.Parse).ToList());
+                if (rows.Count == matrixHeight)
+                {
+                    matrices.Add(new PressureMatrix(rows, DateTime.Now));
+                    rows.Clear();
+                }
+            }
+
+            // Interpolate matrix timestamps throughout day
+            string? dateString = Path.GetFileNameWithoutExtension(dataFilePath)
+                .Split("_").ElementAtOrDefault(1);
+            if (dateString == null) return;
+
+            int secondsInDay = 24 * 60 * 60;
+
+            try
+            {
+                DateTime date = DateTime.ParseExact(
+                    dateString, "yyyyMMdd", CultureInfo.InvariantCulture
+                );
+
+                for (int i = 0; i < matrices.Count; i++)
+                {
+                    matrices[i].Timestamp = date.AddSeconds((secondsInDay / matrices.Count) * i);
+                }
+            }
+            catch
+            {
+                return;
             }
         }
 
         public void Print()
         {
-            Debug.WriteLine(data.Count());
-
-            foreach (List<short> row in data)
+            foreach (PressureMatrix matrix in matrices)
             {
-                
+                Debug.WriteLine(matrix.Timestamp.ToString());
             }
         }
     }
---
+++ software engineering/Lib/PressureMatrix.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+
+namespace software_engineering.Lib
+{
+    public class PressureMatrix
+    {
+        private List<List<short>> Data;
+
+        public DateTime Timestamp;
+
+        public PressureMatrix(List<List<short>> data, DateTime timestamp)
+        {
+            this.Data = data;
+            this.Timestamp = timestamp;
+        }
+
+        public List<List<Vector2>> GetHighPressureRegions(int tolerance = 20)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetPeakPressureIndex()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetContactAreaPercentage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
