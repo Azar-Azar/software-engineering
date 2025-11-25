8696589287f1b25da24aa68203314d03747a5b10
wc296 <wc296@ARU-S-55LTRN3>
2025-11-25 10:35:56 +0000

    feat: finish high pressure regions algorithm

--- software engineering/Program.cs
+++ software engineering/Program.cs
@@ -1,9 +1,28 @@
 using Microsoft.EntityFrameworkCore;
 using software_engineering.Data;
 using software_engineering.Lib;
+using System.Diagnostics;
 
-var metrics = new Metrics(@"C:\Users\wc296\AppData\Local\Development\SampleData\GTLB-Data\1c0fd777_20251011.csv");
-metrics.Print();
+//var metrics = new Metrics(@"C:\Users\wc296\AppData\Local\Development\SampleData\GTLB-Data\1c0fd777_20251011.csv");
+
+//Debug.WriteLine(metrics.matrices.Count);
+//Debug.WriteLine(metrics.matrices.ElementAt(0));
+//Debug.WriteLine(metrics.matrices.ElementAt(0).GetHighPressureRegions(30, 20).Count);
+
+//Debug.WriteLine("first matrix:");
+//metrics.matrices.ElementAt(0).Print();
+
+//foreach (List<PressurePoint> area in metrics.matrices.ElementAt(0).GetHighPressureRegions(30, 20))
+//{
+//    string areaString = "";
+
+//    foreach (PressurePoint point in area)
+//    {
+//        areaString += "(" + point.x.ToString() + ", " + point.y.ToString() + "), ";
+//    }
+
+//    Debug.WriteLine(areaString);
+//}
 
 var builder = WebApplication.CreateBuilder(args);
 
